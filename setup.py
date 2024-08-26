57956a9f2e03b085ac26f6c7e60b419bfadaa153
Jackie <jackie.blaum@gmail.com>
2024-08-26 13:36:42 -0700

    Fill out requirements and setup files.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Read the contents of the README file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="eclipsebin",
+    version="0.1.0",
+    author="Jackie Blaum",
+    author_email="jackie.blaum@example.com",
+    description="A specialized binning scheme for eclipsing binary star light curves",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackieblaum/eclipsebin",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+    ],
+)
