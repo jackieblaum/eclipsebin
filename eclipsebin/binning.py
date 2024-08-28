Fix out-of-eclipse-bins and break up bin calculation into multiple functions.

eclipsebin/binning.py
@@ -35,11 +35,11 @@ class EclipsingBinaryBinner:
             fluxes (np.ndarray): Array of flux values.
             flux_errors (np.ndarray): Array of flux errors.
             nbins (int, optional): Number of bins to use. Defaults to 200.
-            fraction_in_eclipse (float, optional): Fraction of bins within eclipses. 
+            fraction_in_eclipse (float, optional): Fraction of bins within eclipses.
                 Defaults to 0.2.
 
         Raises:
-            ValueError: If the number of data points is less than 10, or if the number of bins 
+            ValueError: If the number of data points is less than 10, or if the number of bins
                 is less than 10, or if the number of data points is less than the number of bins.
         """
         if len(phases) < 10:
@@ -51,15 +51,8 @@ class EclipsingBinaryBinner:
                 "Number of data points must be greater than or equal to the number of bins."
             )
 
-        self.data = {
-            'phases': phases,
-            'fluxes': fluxes,
-            'flux_errors': flux_errors
-        }
-        self.params = {
-            'nbins': nbins,
-            'fraction_in_eclipse': fraction_in_eclipse
-        }
+        self.data = {"phases": phases, "fluxes": fluxes, "flux_errors": flux_errors}
+        self.params = {"nbins": nbins, "fraction_in_eclipse": fraction_in_eclipse}
 
         # Identify primary and secondary eclipse minima
         self.primary_eclipse_min_phase = self.find_minimum_flux()
@@ -80,8 +73,8 @@ class EclipsingBinaryBinner:
         Returns:
             float: Phase value of the primary eclipse minimum.
         """
-        idx_min = np.argmin(self.data['fluxes'])
-        return self.data['phases'][idx_min]
+        idx_min = np.argmin(self.data["fluxes"])
+        return self.data["phases"][idx_min]
 
     def find_secondary_minimum(self):
         """
@@ -91,9 +84,9 @@ class EclipsingBinaryBinner:
         Returns:
             float: Phase value of the secondary eclipse minimum.
         """
-        mask = np.abs(self.data['phases'] - self.primary_eclipse_min_phase) > 0.2
-        idx_secondary_min = np.argmin(self.data['fluxes'][mask])
-        return self.data['phases'][mask][idx_secondary_min]
+        mask = np.abs(self.data["phases"] - self.primary_eclipse_min_phase) > 0.2
+        idx_secondary_min = np.argmin(self.data["fluxes"][mask])
+        return self.data["phases"][mask][idx_secondary_min]
 
     def get_eclipse_boundaries(self, eclipse_min_phase):
         """
@@ -106,7 +99,7 @@ class EclipsingBinaryBinner:
             tuple: Start and end phases of the eclipse.
         """
         start_idx, end_idx = self._find_eclipse_boundaries(eclipse_min_phase)
-        return (self.data['phases'][start_idx], self.data['phases'][end_idx])
+        return (self.data["phases"][start_idx], self.data["phases"][end_idx])
 
     def _find_eclipse_boundaries(self, eclipse_min_phase):
         """
@@ -124,7 +117,7 @@ class EclipsingBinaryBinner:
 
     def _find_eclipse_boundary(self, eclipse_min_phase, direction):
         """
-        Finds the boundary index of an eclipse either before (start) or after (end) 
+        Finds the boundary index of an eclipse either before (start) or after (end)
             the minimum flux.
 
         Args:
@@ -135,31 +128,32 @@ class EclipsingBinaryBinner:
             int: Index of the boundary point.
         """
         if direction == "start":
-            mask = self.data['phases'] < eclipse_min_phase
+            mask = self.data["phases"] < eclipse_min_phase
         else:  # direction == 'end'
-            mask = self.data['phases'] > eclipse_min_phase
+            mask = self.data["phases"] > eclipse_min_phase
 
-        idx_boundary = np.where(mask & np.isclose(self.data['fluxes'], 1.0, atol=0.01))[0]
+        idx_boundary = np.where(mask & np.isclose(self.data["fluxes"], 1.0, atol=0.01))[
+            0
+        ]
 
         if len(idx_boundary) == 0:
             # If no boundary found, use the closest point to 1.0 flux
             if direction == "start":
-                return np.where(np.isclose(self.data['fluxes'], 1.0, atol=0.01))[0][-1]
-            return np.where(np.isclose(self.data['fluxes'], 1.0, atol=0.01))[0][0]
+                return np.where(np.isclose(self.data["fluxes"], 1.0, atol=0.01))[0][-1]
+            return np.where(np.isclose(self.data["fluxes"], 1.0, atol=0.01))[0][0]
         # Return the last or first index depending on direction
         return idx_boundary[-1] if direction == "start" else idx_boundary[0]
 
-    def calculate_bins(self):
+    def find_bin_edges(self):
         """
-        Calculates the bin centers, means, and standard deviations for the binned light curve.
-
-        Returns:
-            tuple: Arrays of bin centers, bin means, bin standard deviations, bin numbers, 
-                and bin edges.
+        Finds the bin edges within the light curve.
         """
-        bins_in_primary = int((self.params['nbins'] * self.params['fraction_in_eclipse']) / 2)
+        bins_in_primary = int(
+            (self.params["nbins"] * self.params["fraction_in_eclipse"]) / 2
+        )
         bins_in_secondary = int(
-            (self.params['nbins'] * self.params['fraction_in_eclipse']) - bins_in_primary
+            (self.params["nbins"] * self.params["fraction_in_eclipse"])
+            - bins_in_primary
         )
 
         primary_bin_edges = self.calculate_eclipse_bins(
@@ -178,12 +172,23 @@ class EclipsingBinaryBinner:
                 (primary_bin_edges, secondary_bin_edges, ooe1_bins, ooe2_bins)
             )
         )
+        return all_bins
+
+    def calculate_bins(self):
+        """
+        Calculates the bin centers, means, and standard deviations for the binned light curve.
+
+        Returns:
+            tuple: Arrays of bin centers, bin means, bin standard deviations, bin numbers,
+                and bin edges.
+        """
+        all_bins = self.find_bin_edges()
         bin_means, bin_edges, bin_number = stats.binned_statistic(
-            self.data['phases'], self.data['fluxes'], statistic="mean", bins=all_bins
+            self.data["phases"], self.data["fluxes"], statistic="mean", bins=all_bins
         )
         bin_centers = (bin_edges[1:] - bin_edges[:-1]) / 2 + bin_edges[:-1]
         bin_stds, _, bin_number = stats.binned_statistic(
-            self.data['phases'], self.data['fluxes'], statistic="std", bins=all_bins
+            self.data["phases"], self.data["fluxes"], statistic="std", bins=all_bins
         )
 
         return bin_centers, bin_means, bin_stds, bin_number, bin_edges
@@ -199,16 +204,19 @@ class EclipsingBinaryBinner:
         Returns:
             np.ndarray: Array of bin edges within the eclipse.
         """
-        start_idx, end_idx = np.searchsorted(self.data['phases'], eclipse_boundaries)
+        start_idx, end_idx = np.searchsorted(self.data["phases"], eclipse_boundaries)
         eclipse_phases = (
             np.concatenate(
-                (self.data['phases'][start_idx:], self.data['phases'][: end_idx + 1] + 1)
+                (
+                    self.data["phases"][start_idx:],
+                    self.data["phases"][: end_idx + 1] + 1,
+                )
             )
             if end_idx < start_idx
-            else self.data['phases'][start_idx : end_idx + 1]
+            else self.data["phases"][start_idx : end_idx + 1]
         )
         bins = pd.qcut(eclipse_phases, q=bins_in_eclipse)
-        return np.array([interval.right for interval in np.unique(bins)]) % 1
+        return np.array([interval.right for interval in np.unique(bins)])
 
     def calculate_out_of_eclipse_bins(self, bins_in_primary, bins_in_secondary):
         """
@@ -221,42 +229,56 @@ class EclipsingBinaryBinner:
         Returns:
             tuple: Arrays of bin edges for the two out-of-eclipse regions.
         """
-        bins_in_ooe1 = int((self.params['nbins'] - bins_in_primary - bins_in_secondary) / 2)
-        bins_in_ooe2 = self.params['nbins'] - bins_in_primary - bins_in_secondary - bins_in_ooe1
-
-        ooe1_bins = pd.qcut(
-            np.concatenate(
-                (
-                    self.data['phases'][
-                        np.searchsorted(self.data['phases'], self.secondary_eclipse[1]) :
-                    ],
-                    self.data['phases'][
-                        : np.searchsorted(self.data['phases'], self.primary_eclipse[0])
-                    ] + 1,
-                )
-            ),
-            q=bins_in_ooe1,
+        bins_in_ooe1 = int(
+            (self.params["nbins"] - bins_in_primary - bins_in_secondary) / 2
         )
-        ooe1_edges = (
-            np.array([interval.right for interval in np.unique(ooe1_bins)])[:-1] % 1
+        bins_in_ooe2 = (
+            self.params["nbins"] - bins_in_primary - bins_in_secondary - bins_in_ooe1
         )
 
-        ooe2_bins = pd.qcut(
+        # Calculate bin edges between end of secondary eclipse and start of primary eclipse
+        end_idx_secondary_eclipse = np.searchsorted(
+            self.data["phases"], self.secondary_eclipse[1]
+        )
+        start_idx_primary_eclipse = np.searchsorted(
+            self.data["phases"], self.primary_eclipse[0]
+        )
+        ooe1_phases = (
             np.concatenate(
                 (
-                    self.data['phases'][
-                        np.searchsorted(self.data['phases'], self.primary_eclipse[1]) :
-                    ],
-                    self.data['phases'][
-                        : np.searchsorted(self.data['phases'], self.secondary_eclipse[0])
-                    ] + 1,
+                    self.data["phases"][end_idx_secondary_eclipse:],
+                    self.data["phases"][: start_idx_primary_eclipse + 1] + 1,
                 )
-            ),
-            q=bins_in_ooe2 + 2,
+            )
+            if end_idx_secondary_eclipse > start_idx_primary_eclipse
+            else self.data["phases"][
+                end_idx_secondary_eclipse : start_idx_primary_eclipse + 1
+            ]
         )
-        ooe2_edges = (
-            np.array([interval.right for interval in np.unique(ooe2_bins)])[:-1] % 1
+        ooe1_bins = pd.qcut(ooe1_phases, q=bins_in_ooe1)
+        ooe1_edges = np.array([interval.right for interval in np.unique(ooe1_bins)])
+
+        # Calculate bin edges between end of primary eclipse and start of secondary eclipse
+        end_idx_primary_eclipse = np.searchsorted(
+            self.data["phases"], self.primary_eclipse[1]
         )
+        start_idx_secondary_eclipse = np.searchsorted(
+            self.data["phases"], self.secondary_eclipse[0]
+        )
+        ooe2_phases = (
+            np.concatenate(
+                (
+                    self.data["phases"][end_idx_primary_eclipse:],
+                    self.data["phases"][: start_idx_secondary_eclipse + 1] + 1,
+                )
+            )
+            if end_idx_primary_eclipse > start_idx_secondary_eclipse
+            else self.data["phases"][
+                end_idx_primary_eclipse : start_idx_secondary_eclipse + 1
+            ]
+        )
+        ooe2_bins = pd.qcut(ooe2_phases, q=bins_in_ooe2)
+        ooe2_edges = np.array([interval.right for interval in np.unique(ooe2_bins)])
 
         return ooe1_edges, ooe2_edges
 
@@ -272,7 +294,7 @@ class EclipsingBinaryBinner:
         """
         plt.figure(figsize=(20, 5))
         plt.errorbar(bin_centers, bin_means, yerr=bin_stds, fmt=".", color="red")
-        plt.scatter(self.data['phases'], self.data['fluxes'], s=20)
+        plt.scatter(self.data["phases"], self.data["fluxes"], s=20)
         plt.vlines(
             self.primary_eclipse, ymin=0.6, ymax=1.1, linestyle="--", color="red"
         )
@@ -289,13 +311,15 @@ class EclipsingBinaryBinner:
         Plots the unbinned light curve with the calculated eclipse minima and bin edges.
         """
         plt.figure(figsize=(20, 5))
-        plt.scatter(self.data['phases'], self.data['fluxes'], s=3)
-        plt.scatter(self.primary_eclipse_min_phase, min(self.data['fluxes']), c="red", s=5)
+        plt.scatter(self.data["phases"], self.data["fluxes"], s=3)
+        plt.scatter(
+            self.primary_eclipse_min_phase, min(self.data["fluxes"]), c="red", s=5
+        )
         plt.scatter(
             self.secondary_eclipse_min_phase,
             min(
-                self.data['fluxes'][
-                    np.abs(self.data['phases'] - self.secondary_eclipse_min_phase) > 0.2
+                self.data["fluxes"][
+                    np.abs(self.data["phases"] - self.secondary_eclipse_min_phase) > 0.2
                 ]
             ),
             c="red",
@@ -315,7 +339,7 @@ class EclipsingBinaryBinner:
         Bins the light curve data and optionally plots the results.
 
         Args:
-            plot (bool, optional): Whether to plot the binned and unbinned light curves. 
+            plot (bool, optional): Whether to plot the binned and unbinned light curves.
              Defaults to True.
 
         Returns:
@@ -328,4 +352,3 @@ class EclipsingBinaryBinner:
             self.plot_unbinned_light_curve()
 
         return bin_centers, bin_means, bin_stds
-    