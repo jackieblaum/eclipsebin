7654424d3b33793f4081b1ad98e8d44fe38cbba3
Jackie <jackie.blaum@gmail.com>
2024-09-25 12:12:52 -0700

    Recalculate eclipse boundaries after setting atol.

--- eclipsebin/binning.py
+++ eclipsebin/binning.py
@@ -272,6 +272,8 @@ class EclipsingBinaryBinner:
             self.params["atol_primary"] = primary
         if secondary is not None:
             self.params["atol_secondary"] = secondary
+        self.primary_eclipse = self.get_eclipse_boundaries(primary=True)
+        self.secondary_eclipse = self.get_eclipse_boundaries(primary=False)
         return 0
 
     def get_atol(self, min_flux):
