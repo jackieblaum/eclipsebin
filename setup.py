664785854a296e5bac23c637742fb96fcbd212e8
Jackie <jackie.blaum@gmail.com>
2024-09-25 11:26:00 -0700

    Update version to 0.2.4.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="eclipsebin",
-    version="0.2.3",
+    version="0.2.4",
     author="Jackie Blaum",
     author_email="jackie.blaum@gmail.com",
     description="A specialized binning scheme for eclipsing binary star light curves",
